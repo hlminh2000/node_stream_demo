7f8a831081b7ecfda2e76d6cf3c9ebd057fd641e
Minh Ha <hlminh2000@gmail.com>
2022-04-03 20:55:49 +0000

    implements everything!

---
+++ index.ts
@@ -0,0 +1,65 @@
+import express from "express";
+import Queue from "promise-queue";
+import fs from "fs/promises";
+import { v4 as uuidv4 } from "uuid";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const queue = new Queue(1);
+const createNumStream = async function* ({
+  shouldContinue,
+}: {
+  shouldContinue: () => boolean;
+}) {
+  const getNextNum = async () => {
+    const { num } = JSON.parse(await fs.readFile("./state.json", "utf-8")) as {
+      num: number;
+    };
+    await fs.writeFile(
+      "./state.json",
+      JSON.stringify({ num: num + 1 }),
+      "utf-8"
+    );
+    return num;
+  };
+
+  while (shouldContinue()) {
+    const nextNum = await queue.add<number>(getNextNum);
+    yield nextNum;
+  }
+};
+
+const streamHandler = async (req, res) => {
+  const requestId = uuidv4();
+  const size = parseInt(req.params.size) || Infinity;
+  const streamState = { connected: true, sentAmount: 0 };
+  const onConnectionClose = () => {
+    streamState.connected = false;
+    console.log(`request ${requestId} closed!`);
+    res.removeListener("close", onConnectionClose);
+  };
+  res.addListener("close", onConnectionClose);
+
+  const stream = createNumStream({
+    shouldContinue: () =>
+      streamState.connected && streamState.sentAmount < size,
+  });
+
+  for await (const num of stream) {
+    console.log(`num sent to request ${requestId}:`, num);
+    res.write(`${num}|`);
+    streamState.sentAmount++;
+  }
+
+  res.end();
+};
+
+app.get("/stream", streamHandler);
+app.get("/stream/:size", streamHandler);
+app.use("/", express.static("./static"));
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Node JS Streaming Demo
+
+This is a demo app to showcase streaming through http with Node JS.
+
+Run it:
+
+```sh
+npm ci && npm start
+```
